0f00a1a13f6de24e10f71c3613b4fc503efea209
Sharon Lam <166158778+sharonlamaier@users.noreply.github.com>
2025-08-24 10:32:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import pandas as pd
 from src.data_utils import load_data, rdfp_gen
 from src.model_utils import load_model, predict_proba, predict_activity
 
@@ -6,7 +7,9 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", type=str, required=True, help="Path to dataset")
     parser.add_argument("--model", type=str, required=True,
-                        help="Model to be used")
+                        help="Specify name of model")
+    parser.add_argument("--o", type=str, default="output.txt",
+                        help="Specify name of output file")
     parser.add_argument("--threshold", type=float, default=0.01,
                         help="Threshold for activity")
     return parser.parse_args()
@@ -17,11 +20,13 @@ def main():
     fp = rdfp_gen(data)
     model = load_model(args.model)
     predicted_prob = predict_proba(fp, model)
-    predicted_activity = predict_activity(predicted_prob,args.threshold) 
-    print(f'predicted probabilities: {predicted_prob}')
-    print(f'predicted activities: {predicted_activity}')
-    return predicted_prob, predicted_activity
+    predicted_activity = predict_activity(predicted_prob,args.threshold)
+    output = args.o 
+    #print(f'predicted probabilities: {predicted_prob}')
+    #print(f'predicted activities: {predicted_activity}')
+    df_predictions = pd.DataFrame({"pred_prob": predicted_prob, "pred_activity": predicted_activity})
+    df = pd.concat([data[['SMILES']],df_predictions],axis=1)
+    df.to_csv(output, sep=",", index=False)
 
 if __name__ == "__main__":
-
     main()
